92d43753d233da03609f87b553cb254ec1bda51b
Berke Yay <105660365+Berke-Yay@users.noreply.github.com>
2024-07-30 22:32:13 +0300

    Update EmailServiceImpl.java

--- src/main/java/com/berkeyay/LGSArkadasimOzelDers/service/EmailServiceImpl.java
+++ src/main/java/com/berkeyay/LGSArkadasimOzelDers/service/EmailServiceImpl.java
@@ -17,6 +17,7 @@ import java.util.List;
 public class EmailServiceImpl implements EmailService{
 
     private String rootMail = "lgsarkadasimprojesi@gmail.com";
+    private String rootMailTest = "yayberke2007@gmail.com";
     @Autowired
     private JavaMailSender mailSender;
 
@@ -25,30 +26,56 @@ public class EmailServiceImpl implements EmailService{
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
         helper.setTo(to);
-        helper.setBcc(rootMail);
-        helper.setSubject("LGS Arkadaşım Özel Ders Başvurusu");
-        //helper.setText(buildEmailBody(application));
+        helper.setBcc(rootMailTest);
 
-        String emailContent = "<!DOCTYPE html><html><head><style>"
-                + "body { font-family: 'Times New Roman', 'Georgia', serif; }"
-                + "</style></head><body>"
-                + "Sevgili LGS Öğrencisi, <br><br>"
-                + "Özel Ders Başvuru detaylarını aşağıda bulabilirsiniz: <br><br>" +
-                "Seçilen ders: " + application.getChosenClass() + "<br>" +
-                "Seçilen konular: " + application.getFirstChosenTopic() + ", " + application.getSecondChosenTopic() + "<br>" +
-                "Dersin tarihi ve saati: " + application.getDate() + " " + application.getTime() + "<br><br>" +
-                "Öğrencinin adı: " + application.getStudentName() + "<br>" +
-                "Velinin adı: " + application.getParentName() + "<br>" +
-                "İletişim numarası: " + application.getPhoneNumber() + "<br><br>" +
-                "Dersi verecek mentörün okulu: " + "<br><br>" +
-                "1. Tercih: " + application.getFirstPreferredSchool() + "<br>" +
-                "2. Tercih: " + application.getSecondPreferredSchool() + "<br>" +
-                "3. Tercih: " + application.getThirdPreferredSchool() + "<br><br>" +
-                "Ek bilgi: " + application.getAdditionalInformation() + "<br><br>"
-                + "LGS Arkadaşım Ekibi" + "<br>"
-                + "+90 (533) 150 02 97" + "<br>"
-                + "lgsarkadasimprojesi@gmail.com" + "<br>"
-                + "</body></html>";
+        //helper.setText(buildEmailBody(application));
+        String emailContent = "";
+        System.out.println(application.getChosenClass());
+        if("mentorship".equalsIgnoreCase(application.getChosenClass())){
+            helper.setSubject("LGS Arkadaşım Bire Bir Mentörlük Başvurusu");
+            emailContent = "<!DOCTYPE html><html><head><style>"
+                    + "body { font-family: 'Times New Roman', 'Georgia', serif; }"
+                    + "</style></head><body>"
+                    + "Sevgili LGS Öğrencisi, <br><br>"
+                    + "Mentör talebiniz alınmıştır. Ekibimiz size en yakın zamanda konuyla ilgili geri dönüş yapacaktır. Başvuru detaylarını aşağıda bulabilirsiniz: <br><br>" +
+                    "İstenilen mentörün okulu: " + "<br><br>" +
+                    "1. Tercih: " + application.getFirstPreferredSchool() + "<br>" +
+                    "2. Tercih: " + application.getSecondPreferredSchool() + "<br>" +
+                    "3. Tercih: " + application.getThirdPreferredSchool() + "<br><br>" +
+                    "Mentörle ilk görüşmenin tarihi ve saati: " + application.getDate() + " " + application.getTime() + "<br><br>" +
+                    "Öğrencinin adı: " + application.getStudentName() + "<br>" +
+                    "Velinin adı: " + application.getParentName() + "<br>" +
+                    "İletişim numarası: " + application.getPhoneNumber() + "<br><br>" +
+                    "Ek bilgi: " + application.getAdditionalInformation() + "<br><br>" +
+                     "LGS Arkadaşım Ekibi" + "<br>"
+                    + "+90 (533) 150 02 97" + "<br>"
+                    + "lgsarkadasimprojesi@gmail.com" + "<br>"
+                    + "</body></html>";
+        }
+        else{
+            helper.setSubject("LGS Arkadaşım Özel Ders Başvurusu");
+            emailContent = "<!DOCTYPE html><html><head><style>"
+                    + "body { font-family: 'Times New Roman', 'Georgia', serif; }"
+                    + "</style></head><body>"
+                    + "Sevgili LGS Öğrencisi, <br><br>"
+                    + "Özel Ders Başvuru detaylarını aşağıda bulabilirsiniz: <br><br>" +
+                    "Seçilen ders: " + application.getChosenClass() + "<br>" +
+                    "Seçilen konular: " + application.getFirstChosenTopic() + ", " + application.getSecondChosenTopic() + "<br>" +
+                    "Dersin tarihi ve saati: " + application.getDate() + " " + application.getTime() + "<br><br>" +
+                    "Öğrencinin adı: " + application.getStudentName() + "<br>" +
+                    "Velinin adı: " + application.getParentName() + "<br>" +
+                    "İletişim numarası: " + application.getPhoneNumber() + "<br><br>" +
+                    "Dersi verecek mentörün okulu: " + "<br><br>" +
+                    "1. Tercih: " + application.getFirstPreferredSchool() + "<br>" +
+                    "2. Tercih: " + application.getSecondPreferredSchool() + "<br>" +
+                    "3. Tercih: " + application.getThirdPreferredSchool() + "<br><br>" +
+                    "Ek bilgi: " + application.getAdditionalInformation() + "<br><br>"
+                    + "LGS Arkadaşım Ekibi" + "<br>"
+                    + "+90 (533) 150 02 97" + "<br>"
+                    + "lgsarkadasimprojesi@gmail.com" + "<br>"
+                    + "</body></html>";
+        }
+
 
         helper.setText(emailContent, true);
 
